49e374b3b6c03288d65bac5dbdcd5e00349c55ec
somnathpy <patilsomnath86@gmail.com>
2020-01-28 15:49:41 -0800

    control Flows samples

---
+++ while.py
@@ -0,0 +1,5 @@
+name = ""
+while name != 'Somanath Patil':
+    print("Entre your Name:")
+    name = input()
+print("Thank you!")
---
+++ while_break.py
@@ -0,0 +1,7 @@
+while True:
+    print("Enter your name:")
+    name = input()
+    if name == 'Somanath Patil':
+        print("your name Matched")
+        break
+print("Thank you")
